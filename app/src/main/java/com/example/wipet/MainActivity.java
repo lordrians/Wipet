dfe2ca47e17c9c2565d907cb919ec9c551079e2b
lordrians <ferrianrp@gmail.com>
2020-06-13 23:57:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/wipet/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.wipet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/wipet/fragment/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.example.wipet.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wipet.R;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+
+    }
+}
