f063835ed00e6faec78eb1e7fa28f56539973d2b
lordrians <ferrianrp@gmail.com>
2020-06-15 00:52:08 +0700

    Mau buat detail diskusi

--- app/src/main/java/com/example/wipet/activity/MainActivity.java
+++ app/src/main/java/com/example/wipet/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package com.example.wipet;
+package com.example.wipet.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.ImageView;
+
+import com.example.wipet.R;
 
 public class MainActivity extends AppCompatActivity {
 
