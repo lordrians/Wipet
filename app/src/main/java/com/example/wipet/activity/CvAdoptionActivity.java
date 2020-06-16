c78428a6a2c98ac6841f37ab4d779c52835e51f5
lordrians <ferrianrp@gmail.com>
2020-06-17 04:33:57 +0700

    CV Adoption done

---
+++ app/src/main/java/com/example/wipet/activity/CvAdoptionActivity.java
@@ -0,0 +1,16 @@
+package com.example.wipet.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wipet.R;
+
+public class CvAdoptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cv_adoption);
+    }
+}
