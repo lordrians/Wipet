40f49f291793f64d54e876cf42390aef7bf795cf
lordrians <ferrianrp@gmail.com>
2020-06-17 02:56:13 +0700

    RequestAdoptionActivity done CV Adoption

---
+++ app/src/main/java/com/example/wipet/activity/ListRequestedAdoptionActivity.java
@@ -0,0 +1,16 @@
+package com.example.wipet.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wipet.R;
+
+public class ListRequestedAdoptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_requested_adoption);
+    }
+}
---
+++ app/src/main/java/com/example/wipet/activity/RequestAdoptionActivity.java
@@ -0,0 +1,16 @@
+package com.example.wipet.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wipet.R;
+
+public class RequestAdoptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_adoption);
+    }
+}
