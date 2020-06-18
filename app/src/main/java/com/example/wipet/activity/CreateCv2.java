aa317e61b37faa0a3e19d154330670d8fed3b635
lordrians <ferrianrp@gmail.com>
2020-06-19 01:30:31 +0700

    CreateCV1 CreateCv2 done masuk DetailAdoption

---
+++ app/src/main/java/com/example/wipet/activity/CreateCv2.java
@@ -0,0 +1,16 @@
+package com.example.wipet.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wipet.R;
+
+public class CreateCv2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_cv2);
+    }
+}
